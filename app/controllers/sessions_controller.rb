83ab5da9299c7fe35de4f7d83048c1737368f522
Eleazar Meza <bairesdev_emeza@iseatz.com>
2020-01-15 13:26:06 -0400

    Implemented cookies based sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     @user = User.find_by email: params[:session][:email].downcase
     if @user && @user.authenticate(params[:session][:password])
       log_in @user
+      remember @user
       redirect_to :root
     else
       flash.now[:danger] = 'Invalid email/password'
@@ -13,7 +14,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out if logged_in?
+    log_out(current_user) if logged_in?
     redirect_to :root
   end
 end
